b281088a5a820a1420e78e5a29c35edc3592ff4c
yumlog <yumlog314@gmail.com>
2023-03-16 18:07:50 +0900

    이미지 업데이트

---
+++ src/components/ButtonToggle.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <label :for="id + '-yes'">
+      <input type="radio"
+        id="id + '-yes'"
+        :name="name"
+        :checked="checked === true"
+        @change="$emit('update:checked', true)"
+      >
+      예
+    </label>
+    <label :for="id + '-no'">
+      <input
+        type="radio"
+        :id="id + '-no'"
+        :name="name"
+        :checked="checked === false"
+        @change="$emit('update:checked', false)"
+      >
+      아니오
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueIeMobileButtonToggle',
+
+  props: {
+    checked: {
+      type: Boolean,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
